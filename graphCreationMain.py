5a50061f92151a4ea3e8fe5881feae07573151f5
fubao <freadream@gmail.com>
2017-08-15 13:23:27 -0400

    add a file

---
+++ graphCreationMain.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 15 12:49:10 2017
+
+@author: fubao
+"""
+
+#main function for creation graph data
+from readCityState import readcityStateExl
+
+class graphCreation:
+    
+    x = 1
+
+    def __init__(self):
+      self.x = 1
+      
+      
+    def createNodeIdPlaces():
+        stateCityMap, stateToCountyMap, countyToCityMap = readcityStateExl()
+      
+        
+
+
+def main():
+    x = 1
+    
+if __name__== "__main__":
+  main()
