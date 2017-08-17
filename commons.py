bca1ff460c6b09566de37e69cab78469a560008d
fubao <freadream@gmail.com>
2017-08-17 11:55:01 -0400

    add_function

--- commons.py
+++ commons.py
@@ -6,6 +6,7 @@ Created on Wed Dec 21 11:52:41 2016
 """
 
 import csv
+import numpy as np
 
 #read file
 def mycsv_reader(csv_reader):         #
@@ -31,3 +32,15 @@ def appendStringRowToFileWriterTsv(fd, stringRow):
     #fd = open(outFile,'a')
     fd.write(stringRow)
     #fd.close()
+    
+#transfer object/string to float
+def get_series_ids(x):
+    '''Function returns a pandas series consisting of ids, 
+       corresponding to objects in input pandas series x
+       Example: 
+       get_series_ids(pd.Series(['a','a','b','b','c'])) 
+       returns Series([0,0,1,1,2], dtype=int)'''
+
+    values = np.unique(x)
+    values2nums = dict(zip(values,range(len(values))))
+    return x.replace(values2nums)
